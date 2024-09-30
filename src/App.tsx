f9e7f95343133779bc533c7b16e6a3f2be71c5f1
kimpra <dustmqaksdltkfrlfdldi@gmail.com>
2024-10-01 00:39:11 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled'
+import Spline from '@splinetool/react-spline'
+
+export default function App() {
+  return (
+    <>
+      <Title>Click the Lid</Title>
+      <Spline scene="https://prod.spline.design/spoYgxpBygpmCQNy/scene.splinecode" />
+    </>
+  )
+}
+
+const Title = styled.h1`
+  text-align: center; 
+`
---
+++ src/globalStyles.ts
@@ -0,0 +1,9 @@
+import { css } from '@emotion/react'
+
+const globalStyles = css`
+  body {
+    background-color: #BED7D3;
+  }
+`
+
+export default globalStyles
