44fa56522187ed80884461ac1d8354b8b1a53e02
kimpra <dustmqaksdltkfrlfdldi@gmail.com>
2024-10-01 11:29:12 +0900

    vite config에 outDir 옵션 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ export default function App() {
   return (
     <>
       <Title>Click the Lid!</Title>
-      <p>이거 왜 배포 실패함?</p>
       <Spline scene="https://prod.spline.design/spoYgxpBygpmCQNy/scene.splinecode" />
     </>
   )
